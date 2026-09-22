8913cb5898f3fb424a5c39754e696b0576d5b4ac
Ram-pranesh <rampranesh07@gmail.com>
2026-09-22 22:13:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.18%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,7 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = list(s.split(" "))
+        if len(pattern) != len(s):
+            return False
+        
+        return len(set(pattern)) == len(set(s)) == len(set(zip(pattern,s)))
