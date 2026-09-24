377d50c7837834799ed5185d9e473a07286f173f
Ram-pranesh <rampranesh07@gmail.com>
2026-09-24 20:18:34 +0530

    Time: 23 ms (5.18%) | Memory: 22.3 MB (6.39%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        print(list(zip(s,t)))
+        print(len(set(s)),len(set(t)),len(set(zip(s,t))))
+        return len(set(s))==len(set(t))==len(set(zip(s,t)))
